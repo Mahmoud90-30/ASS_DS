44be47d36d7c9e1312e4f00aad5595d3cb06fa11
Nadosha22 <nadanabilm.2016@gmail.com>
2026-04-28 19:33:31 +0300

    Refactor binary search to count comparisons

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -1,58 +1,48 @@
 #include <iostream>
 using namespace std;
-int binarysearch(int a[],int size,int key,int &compare){
+int comp=0;
+bool countcomp(bool condition){
+    comp++;
+    return condition;
+}
+int binarysearch(int a[],int size,int key){
     int first=0,last=size-1;
-    compare=0;
     while(first<=last){
         int mid=(first+last)/2;
-        compare++;
-        if(a[mid]==key) return mid;
-        compare++;
-        if(a[first]==a[mid]){
-            compare++;
-            if(a[mid]==a[last]){
-                first++;
-                last--;
-                continue;
-            }
+        if(countcomp(a[mid]==key)) return mid;
+        if(countcomp(a[first]==a[mid]) && countcomp(a[mid]==a[last])){
+            first++;
+            last--;
         }
-        compare++;
-        if(a[first]<=a[mid]){
-            compare++;
-            if(a[first]<=key){
-                compare++;
-                if(key<a[mid]) last=mid-1;
-                else first=mid+1;
-            }
+        else if(countcomp(a[first]<=a[mid])){
+            if(countcomp(a[first]<=key) && countcomp(key<a[mid])) last=mid-1;
             else first=mid+1;
         }
         else{
-            compare++;
-            if(a[mid]<key){
-                compare++;
-                if(key<=a[last]) first=mid+1;
-                else last=mid-1;
-            }
+            if(countcomp(a[mid]<key) && countcomp(key<=a[last])) first=mid+1;
             else last=mid-1;
         }
     }
     return -1;
 }
-void result(int item,int res,int com){
+void result(int item,int res){
     if(res==-1) cout<<"Element: "<<item<<" is not found\n";
     else cout<<"Element: "<<item<<" is found\n";
-    cout<<"Index of the element: "<<res<<"\nNumber of comparisons: "<<com<<"\n\n";
+    cout<<"Index of the element: "<<res<<"\nNumber of comparisons: "<<comp<<"\n\n";
 }
 int main(){
     //without duplication
-    int arr[7]={5,6,7,1,2,3,4},comp,item=7;
-    int res=binarysearch(arr,7,item,comp);
-    result(item,res,comp);
+    cout<<"===== Without Duplication =====\n";
+    int arr[7]={5,6,7,1,2,3,4},item=7;
+    int res=binarysearch(arr,7,item);
+    result(item,res);
 
     //with duplication
-    int arr2[7]={1,6,7,1,2,3,1},comp2,item2=7;
-    int res2=binarysearch(arr2,7,item2,comp2);
-    result(item2,res2,comp2);
+    cout<<"===== With Duplication =====\n";
+    comp = 0;
+    int arr2[9]={3,3,3,1,3,3,3,3,3},item2=1;
+    int res2=binarysearch(arr2,9,item2);
+    result(item2,res2);
 
     /*best case o(1), worst case o(n)*/
 }
