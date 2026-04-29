40b65559e0d7685ed05e11460c352d0228b643e3
MIna Hany <minahanyatef2003@gmail.com>
2026-04-29 04:01:00 +0300

    Update RoundRobin.cpp

--- RoundRobin.cpp
+++ RoundRobin.cpp
@@ -1,118 +1,143 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <iomanip>
+#include <climits>
+#include <string>
 using namespace std;
 
-struct Process {
-    int id;
-    int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int completion_time;
-    int turnaround_time;
-    int waiting_time;
+class Process {
+public:
+    int id,
+        arrival_time,
+        burst_time,
+        remaining_time,
+        completion_time,
+        turnaround_time,
+        waiting_time;
+
+    Process(int id, int at, int bt) : 
+        id(id),  
+        arrival_time(at),
+        burst_time(bt),
+        remaining_time(bt),
+        completion_time(0), 
+        turnaround_time(0),
+        waiting_time(0) {}
+
+    bool isDone() { return remaining_time == 0; } 
+    void calcTimes() 
+    { turnaround_time = completion_time - arrival_time; waiting_time = turnaround_time - burst_time; }
+    
+    void print() {      
+        cout << "P" << id << "\t\t"
+            << completion_time << "\t\t"
+            << turnaround_time << "\t\t"
+            << waiting_time << "\n";
+    }
 };
 
-int main() {
+class RoundRobinScheduler {
+private:
+    vector<Process> procs;
+    queue<int> rq;
+    vector<bool> inQ, done;
+    int quantum, time, doneCount;
 
-    int n, quantum;
-    cin >> quantum >> n;
-
-    vector<Process> p(n);
-
-    for (int i = 0; i < n; i++) {
-        cin >> p[i].arrival_time >> p[i].burst_time;
-        p[i].id = i + 1;
-        p[i].remaining_time = p[i].burst_time;
-        p[i].completion_time = 0;
-    }
-
-    queue<int> q;
-    vector<bool> inQueue(n, false);
-
-    int current_time = 0;
-    int completed = 0;
-
-    cout << "Queue updates:\n";
-
-    while (completed < n) {
-
-        for (int i = 0; i < n; i++) {
-            if (p[i].arrival_time <= current_time &&
-                !inQueue[i] &&
-                p[i].remaining_time > 0) {
-
-                q.push(i); 
-                inQueue[i] = true;
-            }
-        }
-
-        
-        if (q.empty()) {  
-            cout << "[ Empty ]\n";
-            current_time++;
-            continue;
-        }
-        queue<int> temp = q;  
+    void printQueue() {
+        if (rq.empty()) { cout << "[ Empty ]\n"; return; }
+        queue<int> tmp = rq;
         cout << "[ ";
-        while (!temp.empty()) {
-            cout << "P" << p[temp.front()].id;
-            temp.pop();
-            if (!temp.empty()) cout << ", ";
+        while (!tmp.empty()) {   
+            cout << "P" << tmp.front();
+            tmp.pop(); 
+            if (!tmp.empty()) cout << ", ";
         }
         cout << " ]\n";
+    }
 
-        
-        int i = q.front();
-        q.pop();
-
-         
-        int exec = min(quantum, p[i].remaining_time); 
-        current_time += exec;
-        p[i].remaining_time -= exec; 
-        
-        for (int j = 0; j < n; j++) {
-            if (p[j].arrival_time <= current_time &&
-                !inQueue[j] &&   
-                p[j].remaining_time > 0) {
-
-                q.push(j);
-                inQueue[j] = true;
+    void addArrivals(int from, int to) {
+        for (int i = 0; i < (int)procs.size(); i++)
+            if (!done[i] && !inQ[i] && procs[i].arrival_time > from && procs[i].arrival_time <= to)
+            { ا
+                rq.push(procs[i].id); inQ[i] = true; 
             }
-        }
+    }
 
-        
-        if (p[i].remaining_time > 0) {
-            q.push(i);    
-        }
-        else {
-            p[i].completion_time = current_time;
-            completed++;    
+    void jumpToNext() {
+        int next = INT_MAX; 
+        for (int i = 0; i < (int)procs.size(); i++)
+            if (!done[i] && !inQ[i]) next = min(next, procs[i].arrival_time);
+        time = next;
+        for (int i = 0; i < (int)procs.size(); i++)
+            if (!done[i] && !inQ[i] && procs[i].arrival_time <= time)  
+            {
+                rq.push(procs[i].id); inQ[i] = true;
+            }
+        printQueue();
+    }
+
+public:
+    RoundRobinScheduler(int q) : quantum(q), time(0), doneCount(0) {}
+
+    void addProcess(int id, int at, int bt) {
+        procs.push_back(Process(id, at, bt));
+        inQ.push_back(false);
+        done.push_back(false);
+    }
+    void run() {
+        int n = procs.size();
+        for (int i = 0; i < n; i++)
+            if (procs[i].arrival_time == 0) { rq.push(procs[i].id); inQ[i] = true; }
+
+        cout << "\nQueue updates:\n"; 
+        if (!rq.empty()) printQueue();
+
+        while (doneCount < n) {
+            if (rq.empty()) { jumpToNext(); continue; }
+            int pid = rq.front(); rq.pop();
+            int idx = pid - 1;
+            int exec = min(procs[idx].remaining_time, quantum); 
+            int prev = time;
+            time += exec;
+            procs[idx].remaining_time -= exec; 
+            addArrivals(prev, time); 
+            if (procs[idx].isDone()) { 
+                procs[idx].completion_time = time;
+                procs[idx].calcTimes();
+                done[idx] = true; inQ[idx] = false; doneCount++;
+            }
+            else {
+                rq.push(pid);
+            }
+            printQueue();
         }
     }
 
-    cout << "[ Empty ]\n";
+    void printResults() {
+        int n = procs.size(), totalWt = 0;  
+        cout << "\nProcess\t\tCompletion Time\t\tTurnaround Time\t\tWaiting Time\n";
+        for (int i = 0; i < n; i++) { procs[i].print();
+        totalWt += procs[i].waiting_time; 
+        }
+        cout << "\nAverage Waiting Time: " << (float)totalWt / n << "\n";
+    }
+};
 
-    
-    float total_waiting = 0;
 
-    cout << "Process Completion Time \n";
+int main() {
+    int n, q;
+    cout << "Time Quantum: "; cin >> q;
+    cout << "Number of Processes: "; cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        p[i].turnaround_time = p[i].completion_time - p[i].arrival_time;
-        p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
-
-        total_waiting += p[i].waiting_time;
-
-        cout << "P" << p[i].id << " "
-            << p[i].completion_time << " "
-            << p[i].turnaround_time << " "
-            << p[i].waiting_time << "\n";
+    RoundRobinScheduler scheduler(q);
+    for (int i = 1; i <= n; i++) {
+        int at, bt;
+        cout << "Arrival Time, Needed Time P" << i << ": "; cin >> at >> bt;
+        scheduler.addProcess(i, at, bt);  
     }
 
-    cout << fixed << setprecision(1);
-    cout << "Average Waiting Time: " << total_waiting / n << endl;
-
+    scheduler.run();
+    scheduler.printResults();
     return 0;
 }
